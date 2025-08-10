2f33d5761b4c07a0127f301db94879105e09eae0
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-08-10 12:05:47 -0700

    fixed tim core fabric being pulled in nf

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit_version")}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${property("junit_version")}")
 
-    modImplementation("maven.modrinth:cobblemon-tim-core:${property("tim_core_fabric_version")}")
+    modImplementation("maven.modrinth:cobblemon-tim-core:${property("tim_core_neoforge_version")}")
 }
 
 tasks.getByName<Test>("test") {
